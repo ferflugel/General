6dab63a12833558ebf375988b8951c0dfb930404
Fernando <fernandofassad@gmail.com>
2021-10-13 08:20:46 -0400

    physics lab

--- Resistance lab/analysis.py
+++ Resistance lab/analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import statsmodels.api as sm
 
 BLUE = (0.12156862745098039, 0.4666666666666667, 0.7058823529411765)
@@ -23,14 +24,26 @@ sns.regplot(data=ammeter, x='I', y='V', order=1, ci=0, line_kws={'lw': 1, 'ls': 
 sns.despine()
 plt.ylim(6.495, 6.505)
 plt.ylabel('V (V)')
-plt.xlabel('I (mA)')
-plt.title('Voltage vs current for Option 1')
+plt.xlabel('I (A)')
+plt.title('V = 6.5000 + 0.0000 I')
+plt.text(0.02, 6.5008, 'R$^2$ = 1.00\nχ$^2$ = 0.00')
+plt.savefig('plots/option_1', dpi=180)
 plt.show()
 
 # Linear regression
 ammeter['const'] = 1
 model = sm.OLS(ammeter['V'], ammeter[['I'] + ['const']]).fit()
 print(model.summary())
+
+# Getting chi square
+def fit_function(x):
+    y = 6.5000 + 0 * x
+    return y
+
+
+X, Y, UNC = np.array(ammeter['I']), np.array(ammeter['V']), np.array(ammeter['dV'])
+print(f'Variance: {Y.std()**2}')
+print(f'Chi square: {chi_square(X, Y, UNC, fit_function)}')
 
 #%% Plots and analysis for the voltmeter
 
@@ -44,11 +57,26 @@ sns.regplot(data=voltmeter, x='I', y='V', order=1, ci=0, line_kws={'lw': 1, 'ls'
 sns.despine()
 plt.ylim(6.495, 6.505)
 plt.ylabel('V (V)')
-plt.xlabel('I (mA)')
-plt.title('Voltage vs current for Option 2')
+plt.xlabel('I (A)')
+plt.title('V = 6.5001 - 0.0334 I')
+plt.text(0.02, 6.5, 'R$^2$ = 0.80\nχ$^2$ = 0.61')
+plt.savefig('plots/option_2', dpi=180)
 plt.show()
 
 # Linear regression
 voltmeter['const'] = 1
 model = sm.OLS(voltmeter['V'], voltmeter[['I'] + ['const']]).fit()
 print(model.summary())
+
+# Finding chi square
+X, Y, UNC = np.array(ammeter['I']), np.array(ammeter['V']), np.array(ammeter['dV'])
+
+# Getting chi square
+def fit_function2(x):
+    y = 6.5001 - 0.0334 * x
+    return y
+
+
+X2, Y2, UNC2 = np.array(voltmeter['I']), np.array(voltmeter['V']), np.array(voltmeter['dV'])
+print(f'Variance: {Y2.std()**2}')
+print(f'Chi square: {chi_square(X2, Y2, UNC2, fit_function2)}')
